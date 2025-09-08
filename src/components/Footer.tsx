29745bdf44a76e4ee73eec19ddabdeb81aaacea0
Hammad Ur Rehman <hammaddevsinn@gmail.com>
2025-09-08 17:56:16 +0500

    added tailwind css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,64 +1,86 @@
 "use client"
 
-import Link from "next/link";
-import "./Footer.css";
+import Link from "next/link"
 
 export default function Footer() {
-    return (
-        <>
-        <div className="footer-container-style-0" data-hs-cos-general-type="widget" data-hs-cos-type="module">
-            <footer className="footer-style-1">
-                <div className="footer-style-2">
-                    <div className="footer-style-3">
-                        <div className="footer-style-4">
-                            <div className="footer-style-5">
-                                <Link href="/" className="footer-style-6">
-                                    <img src="https://www.align.com/hubfs/align-fullcolor-logo.svg" alt="align-fullcolor-logo" loading="lazy" width="136" height="46" className="footer-style-7" />
-                                </Link>
-                            </div>
-                            <div className="footer-style-8">
-                                <span className="footer-style-9" data-hs-cos-general-type="widget" data-hs-cos-type="form">
-                                    <h3 className="footer-style-10" data-hs-cos-general-type="widget_field" data-hs-cos-type="text">Subscribe to our blog!</h3>
-                                    <div data-hs-forms-root="true" className="footer-style-11">
-                                        <form method="POST" acceptCharset="UTF-8" encType="multipart/form-data" noValidate action="https://forms.hsforms.com/submissions/v3/public/submit/formsnext/multipart/3343341/efe473dd-3d0f-4a68-b68d-bf4422eebd5b" className="footer-style-12" target="target_iframe_efe473dd-3d0f-4a68-b68d-bf4422eebd5b_523" data-instance-id="50412b5f-4e48-491c-8789-cdff51061ad5" data-form-id="efe473dd-3d0f-4a68-b68d-bf4422eebd5b" data-portal-id="3343341" data-test-id="hsForm_efe473dd-3d0f-4a68-b68d-bf4422eebd5b_523">
-                                            <div className="footer-style-13"><label className="footer-style-14" htmlFor="email-efe473dd-3d0f-4a68-b68d-bf4422eebd5b_523"><span className="footer-style-15"></span></label>
-                                                <div className="footer-style-16"><input name="email" required placeholder="Email*" type="email" className="footer-style-17" inputMode="email" autoComplete="email" defaultValue="" /></div>
-                                            </div>
-                                            <div className="footer-style-18"><label className="footer-style-19"><span className="footer-style-20">Notification Frequency</span></label>
-                                                <div className="footer-style-21"><input name="blog_default_hubspot_blog_5097508453_subscription" className="footer-style-22" type="hidden" value="instant" /></div>
-                                            </div>
-                                            <div className="footer-style-23">
-                                                <div className="footer-style-24"></div>
-                                                <div className="footer-style-25"><input type="submit" className="footer-style-26" value="Subscribe" /></div>
-                                            </div><input name="hs_context" type="hidden" value="{&quot;embedAtTimestamp&quot;:&quot;1756852081877&quot;,&quot;formDefinitionUpdatedAt&quot;:&quot;1752774973712&quot;,&quot;clonedFromForm&quot;:&quot;be72451d-ee64-4aee-88aa-4e33b59dd7c0&quot;,&quot;userAgent&quot;:&quot;Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36&quot;,&quot;pageTitle&quot;:&quot;Technology Infrastructure Solutions | Align&quot;,&quot;pageUrl&quot;:&quot;https://www.align.com/&quot;,&quot;pageId&quot;:&quot;10658659860&quot;,&quot;isHubSpotCmsGeneratedPage&quot;:true,&quot;formTarget&quot;:&quot;#hs_form_target_form_816182971&quot;,&quot;formInstanceId&quot;:&quot;523&quot;,&quot;rawInlineMessage&quot;:&quot;Thanks for subscribing! &lt;a href=\&quot;https://www.align.com/subscribe-to-our-blog\&quot; rel=\&quot;noopener\&quot; target=\&quot;_blank\&quot;&gt;Manage your preferences here.&lt;/a&gt;&quot;,&quot;hsFormKey&quot;:&quot;377a7aa4813c0b672b1113c738b48bd2&quot;,&quot;contentType&quot;:&quot;standard-page&quot;,&quot;pageName&quot;:&quot;Technology Infrastructure Solutions | Align&quot;,&quot;rumScriptExecuteTime&quot;:1174.6999999955297,&quot;rumTotalRequestTime&quot;:1843.7999999970198,&quot;rumTotalRenderTime&quot;:1887.2999999970198,&quot;rumServiceResponseTime&quot;:669.1000000014901,&quot;rumFormRenderTime&quot;:43.5,&quot;connectionType&quot;:&quot;&quot;,&quot;firstContentfulPaint&quot;:0,&quot;largestContentfulPaint&quot;:0,&quot;locale&quot;:&quot;en&quot;,&quot;timestamp&quot;:1756852082788,&quot;originalEmbedContext&quot;:{&quot;portalId&quot;:&quot;3343341&quot;,&quot;formId&quot;:&quot;efe473dd-3d0f-4a68-b68d-bf4422eebd5b&quot;,&quot;region&quot;:&quot;na1&quot;,&quot;target&quot;:&quot;#hs_form_target_form_816182971&quot;,&quot;isBuilder&quot;:false,&quot;isTestPage&quot;:false,&quot;isPreview&quot;:false,&quot;formInstanceId&quot;:&quot;523&quot;,&quot;formsBaseUrl&quot;:&quot;/_hcms/forms&quot;,&quot;css&quot;:&quot;&quot;,&quot;inlineMessage&quot;:&quot;Thanks for subscribing! &lt;a href=\&quot;https://www.align.com/subscribe-to-our-blog\&quot; rel=\&quot;noopener\&quot; target=\&quot;_blank\&quot;&gt;Manage your preferences here.&lt;/a&gt;&quot;,&quot;isMobileResponsive&quot;:true,&quot;rawInlineMessage&quot;:&quot;Thanks for subscribing! &lt;a href=\&quot;https://www.align.com/subscribe-to-our-blog\&quot; rel=\&quot;noopener\&quot; target=\&quot;_blank\&quot;&gt;Manage your preferences here.&lt;/a&gt;&quot;,&quot;hsFormKey&quot;:&quot;377a7aa4813c0b672b1113c738b48bd2&quot;,&quot;pageName&quot;:&quot;Technology Infrastructure Solutions | Align&quot;,&quot;pageId&quot;:&quot;10658659860&quot;,&quot;contentType&quot;:&quot;standard-page&quot;,&quot;formData&quot;:{&quot;cssClass&quot;:&quot;hs-form stacked hs-custom-form&quot;},&quot;isCMSModuleEmbed&quot;:true},&quot;correlationId&quot;:&quot;50412b5f-4e48-491c-8789-cdff51061ad5&quot;,&quot;renderedFieldsIds&quot;:[&quot;email&quot;,&quot;blog_default_hubspot_blog_5097508453_subscription&quot;],&quot;captchaStatus&quot;:&quot;NOT_APPLICABLE&quot;,&quot;emailResubscribeStatus&quot;:&quot;NOT_APPLICABLE&quot;,&quot;isInsideCrossOriginFrame&quot;:false,&quot;source&quot;:&quot;forms-embed-1.9606&quot;,&quot;sourceName&quot;:&quot;forms-embed&quot;,&quot;sourceVersion&quot;:&quot;1.9606&quot;,&quot;sourceVersionMajor&quot;:&quot;1&quot;,&quot;sourceVersionMinor&quot;:&quot;9606&quot;,&quot;allPageIds&quot;:{&quot;embedContextPageId&quot;:&quot;10658659860&quot;,&quot;contentPageId&quot;:10658659860,&quot;contentAnalyticsPageId&quot;:&quot;10658659860&quot;},&quot;_debug_embedLogLines&quot;:[{&quot;clientTimestamp&quot;:1756852082741,&quot;level&quot;:&quot;INFO&quot;,&quot;message&quot;:&quot;Retrieved customer callbacks used on embed context: [\&quot;getExtraMetaDataBeforeSubmit\&quot;]&quot;},{&quot;clientTimestamp&quot;:1756852082742,&quot;level&quot;:&quot;INFO&quot;,&quot;message&quot;:&quot;Retrieved pageContext values which may be overriden by the embed context: {\&quot;pageTitle\&quot;:\&quot;Technology Infrastructure Solutions | Align\&quot;,\&quot;pageUrl\&quot;:\&quot;https://www.align.com/\&quot;,\&quot;userAgent\&quot;:\&quot;Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36\&quot;,\&quot;pageId\&quot;:\&quot;10658659860\&quot;,\&quot;contentAnalyticsPageId\&quot;:\&quot;10658659860\&quot;,\&quot;contentPageId\&quot;:10658659860,\&quot;isHubSpotCmsGeneratedPage\&quot;:true}&quot;},{&quot;clientTimestamp&quot;:1756852082743,&quot;level&quot;:&quot;INFO&quot;,&quot;message&quot;:&quot;Retrieved countryCode property from normalized embed definition response: \&quot;PK\&quot;&quot;}]}" className="footer-style-27" /><iframe name="target_iframe_efe473dd-3d0f-4a68-b68d-bf4422eebd5b_523" className="footer-style-28"></iframe>
-                                        </form>
-                                    </div>
-                                </span>
-                            </div>
-                        </div>
-                        <div className="footer-style-29">
-                            <div className="footer-style-30"></div>
-                            <div className="footer-style-31">
-                                <a href="https://www.align.com/solutions/data-center-solutions?hsLang=en" target="" className="footer-style-32">Data Center Infrastructure Solutions</a> <a href="https://www.align.com/professional-services/workplace-technology?hsLang=en" target="" className="footer-style-33">Workplace Technology</a> <a href="https://www.align.com/managed-services?hsLang=en" target="" className="footer-style-34">Managed Services</a> <a href="https://www.align.com/about?hsLang=en" target="" className="footer-style-35">About Align</a> <a href="https://www.align.com/align-locations?hsLang=en" target="" className="footer-style-36">Locations</a> <a href="https://www.align.com/press?hsLang=en" target="" className="footer-style-37">Press</a>
-                            </div>
-                            <div className="footer-style-38">
-                                <a href="https://www.align.com/resource-library?hsLang=en" target="" className="footer-style-39">Resource Center</a> <a href="https://www.align.com/blog?hsLang=en" target="" className="footer-style-40">Blog</a> <a href="https://www.align.com/success-stories?hsLang=en" target="" className="footer-style-41">Success Stories</a> <a href="https://www.align.com/align-partners-clients?hsLang=en" target="" className="footer-style-42">Clients and Partners</a> <a href="https://www.align.com/leadership?hsLang=en" target="" className="footer-style-43">Leadership</a> <a href="https://www.align.com/it-careers-align?hsLang=en" target="" className="footer-style-44">Careers</a> <a href="https://www.align.com/awards?hsLang=en" target="" className="footer-style-45">Awards</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <div className="footer-style-46">
-                <div className="footer-style-47">
-                    <div className="footer-style-48">
-                        <a href="https://www.align.com/legal?hsLang=en" target="" className="footer-style-49">Privacy</a> <span className="footer-style-50">&nbsp;|&nbsp;</span> <a href="https://www.align.com/billing_tc?hsLang=en" target="" className="footer-style-51">Terms and Conditions</a>
-                    </div>
-                    <div className="footer-style-52">
-                        ©2025 | All Rights Reserved | Align Communications, Inc.
-                    </div>
-                </div>
+  return (
+    <div className="box-border">
+      {/* Main Footer */}
+      <footer className="max-w-[1200px] mx-auto w-full px-6 sm:px-9 py-[54px]">
+        <div className="flex flex-col md:flex-row md:flex-wrap gap-y-12 md:gap-y-0 gap-x-[3.3333%] max-w-[1500px] mx-auto">
+          
+          {/* Logo + Subscribe */}
+          <div className="w-full md:w-[31.11%]">
+            <Link href="/" className="inline-block">
+              <img
+                src="https://www.align.com/hubfs/align-fullcolor-logo.svg"
+                alt="align-fullcolor-logo"
+                width="136"
+                height="46"
+                className="max-w-full h-auto"
+              />
+            </Link>
+
+            {/* Subscribe */}
+            <div className="mt-8 mb-6">
+              <h3 className="text-[18px] font-semibold leading-[28px] font-['IBM Plex Sans'] mb-3">
+                Subscribe to our blog!
+              </h3>
+              <form className="flex flex-col sm:flex-row gap-3 sm:gap-0">
+                {/* Email input */}
+                <input
+                  type="email"
+                  placeholder="Email*"
+                  required
+                  className="h-[48px] w-full px-5 text-[18px] font-['IBM Plex Sans'] border-2 border-[#00d1ff] rounded-md sm:rounded-l-md sm:rounded-r-none outline-none"
+                />
+
+                {/* Button */}
+                <button
+                  type="submit"
+                  className="bg-gradient-to-r from-[#008ad4] to-[#00d1ff] h-[48px] px-6 text-[18px] font-semibold rounded-md sm:rounded-r-md sm:rounded-l-none text-white shadow-md hover:opacity-90 transition"
+                >
+                  Subscribe
+                </button>
+              </form>
             </div>
+          </div>
+
+          {/* Footer Links */}
+          <div className="w-full md:w-[65.55%] flex flex-col sm:flex-row gap-8 md:gap-[3.3333%]">
+            <div className="w-full sm:w-[48.33%] flex flex-col gap-4 text-[18px] font-semibold">
+              <a href="https://www.align.com/solutions/data-center-solutions?hsLang=en">Data Center Infrastructure Solutions</a>
+              <a href="https://www.align.com/professional-services/workplace-technology?hsLang=en">Workplace Technology</a>
+              <a href="https://www.align.com/managed-services?hsLang=en">Managed Services</a>
+              <a href="https://www.align.com/about?hsLang=en">About Align</a>
+              <a href="https://www.align.com/align-locations?hsLang=en">Locations</a>
+              <a href="https://www.align.com/press?hsLang=en">Press</a>
+            </div>
+
+            <div className="w-full sm:w-[31.11%] flex flex-col gap-4 text-[18px] font-semibold">
+              <a href="https://www.align.com/resource-library?hsLang=en">Resource Center</a>
+              <a href="https://www.align.com/blog?hsLang=en">Blog</a>
+              <a href="https://www.align.com/success-stories?hsLang=en">Success Stories</a>
+              <a href="https://www.align.com/align-partners-clients?hsLang=en">Clients and Partners</a>
+              <a href="https://www.align.com/leadership?hsLang=en">Leadership</a>
+              <a href="https://www.align.com/it-careers-align?hsLang=en">Careers</a>
+              <a href="https://www.align.com/awards?hsLang=en">Awards</a>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </footer>
+
+      {/* Secondary Footer */}
+      <div className="bg-[#f3f3f3] py-9 text-[12px] font-semibold font-['IBM Plex Sans']">
+        <div className="max-w-[1200px] mx-auto px-6 sm:px-9 flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+          <div className="flex items-center gap-2">
+            <a href="https://www.align.com/legal?hsLang=en">Privacy</a>
+            <span>|</span>
+            <a href="https://www.align.com/billing_tc?hsLang=en">Terms and Conditions</a>
+          </div>
+          <div>©2025 | All Rights Reserved | Align Communications, Inc.</div>
+        </div>
+      </div>
+    </div>
+  )
 }
